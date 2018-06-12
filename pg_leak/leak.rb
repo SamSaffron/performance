c1efb116a178afe4d5683956ec27b3027003191a
Sam <sam.saffron@gmail.com>
2018-06-13 09:38:05 +1000

    demo the leak when not clearing

---
+++ pg_leak/leak.rb
@@ -0,0 +1,84 @@
+# frozen_string_literal: true
+# tiny rerunner (I use this on local to rerun script)
+require_relative '../rerunner/rerunner.rb'
+
+require 'bundler/inline'
+
+gemfile do
+  source 'https://rubygems.org'
+  gem 'pg'
+end
+
+require 'pg'
+
+def count_malloc(desc)
+  start = GC.stat[:malloc_increase_bytes]
+  yield
+  delta = GC.stat[:malloc_increase_bytes] - start
+  puts "#{desc} allocated #{delta} bytes"
+end
+
+def process_rss
+  puts 'RSS is: ' + `ps -o rss -p #{$$}`.chomp.split("\n").last
+end
+
+conn = PG.connect(dbname: 'test_db')
+sql = "select repeat('x', $1)"
+
+count_malloc("100,000 bytes PG") do
+  conn.exec(sql, [100_000])
+end
+
+count_malloc("100,000 byte string") do
+  "x" * 100_000
+end
+
+# simulate a Rails app by long term retaining 400_000 objects
+$long_term = []
+400_000.times do
+  $long_term << +""
+end
+
+puts "start RSS"
+process_rss
+
+x = []
+10_000.times do |i|
+  x[i%10]  = "x" * 100_000
+end
+
+
+puts "RSS after allocating 10k 100,000 byte string"
+process_rss
+
+10_000.times do |i|
+  r = x[i%10] = conn.exec(sql, [100_000])
+  r.clear
+end
+
+puts "RSS after allocating 10k 100,000 byte strings in libpq (and clearing)"
+process_rss
+
+10_000.times do |i|
+  x[i%10] = conn.exec(sql, [100_000])
+end
+
+puts "RSS after allocating 10k 100,000 byte strings in libpq (and NOT clearing)"
+process_rss
+puts "done"
+
+
+# 100,000 bytes PG allocated 960 bytes
+# 100,000 byte string allocated 103296 bytes
+#
+# start RSS
+# RSS is: 48492
+#
+# RSS after allocating 10k 100,000 byte string
+# RSS is: 97752
+#
+# RSS after allocating 10k 100,000 byte strings in libpq (and clearing)
+# RSS is: 100656
+#
+# RSS after allocating 10k 100,000 byte strings in libpq (and NOT clearing)
+# RSS is: 1141768
