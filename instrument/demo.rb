46596b16e84c5a8b91d7f0a664de05524d4edf47
Sam Saffron <sam.saffron@gmail.com>
2019-11-17 10:45:13 +1100

    basic work on instruments

--- instrument/demo.rb
+++ instrument/demo.rb
@@ -1,14 +1,36 @@
 # frozen_string_literal: true
+require 'bundler/inline'
+
+gemfile do
+  source 'https://rubygems.org'
+  gem 'activesupport', path: '../../rails/activesupport'
+  gem 'benchmark-ips'
+end
 
 require 'active_support'
 
 class NotificationSubscriber
   def self.add_instrument(klass, method, name)
-    @subscriptions ||= {}
-    @subscriptions[name] = {klass: klass, method: method}
+    @instruments ||= {}
+    @instruments[name] = { klass: klass, method: method }
   end
 
   def self.subscribe(name, &blk)
+
+    info = @instruments[name]
+    raise "nothing to subscribe to" if !info
+
+    patch = <<~RUBY
+      alias_method #{info[:method]}, #{info[:method]}_orig
+      def #{info[:method]}(a: )
+        #{info[:method]}_orig(a: a)
+
+        @instrument
+      end
+    RUBY
+
+    info[:klass].class_eval patch
+
   end
 end
 
@@ -20,14 +42,14 @@ end
 
 class Test
   instrument(:foo, 'abc/test')
-  def foo(a, b: , c: 1, &blk)
-    1
+  def empty_v2(a:)
   end
 
-  def foo2
-    ActiveSupport::Notifications.instrument("abc/test", a: 1) do |x|
-      p x
-      1
+  def empty(a:)
+  end
+
+  def empty_as(a:)
+    ActiveSupport::Notifications.instrument("abc/test", a: a) do |x|
     end
   end
 end
@@ -36,17 +58,31 @@ NotificationSubscriber.subscribe('abc/test') do |x|
   p x
 end
 
-ActiveSupport::Notifications.subscribe("abc/test") do |*x|
-  p x
+ActiveSupport::Notifications.subscribe("abc/test") do |x|
 end
-
-Test.new.foo2
 
 class Test
-  p instance_method(:foo).parameters
+  #p instance_method(:foo).parameters
 end
 
+t = Test.new
 
+t.empty_v2(a: 1)
+exit
 
+Benchmark.ips do |x|
+  x.report("empty") do |times|
+    while times > 0
+      t.empty(a: 1)
+      times -= 1
+    end
+  end
 
+  x.report("empty_as") do |times|
+    while times > 0
+      t.empty_as(a: 1)
+      times -= 1
+    end
+  end
 
+end
